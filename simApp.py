df0b377d249bab1838ecceda9e5b4a5fdcf3ad18
orchardlinc <165101795+orchardlinc@users.noreply.github.com>
2025-04-02 08:14:26 -0400

    Update simApp.py

--- simApp.py
+++ simApp.py
@@ -66,7 +66,7 @@ def check_transactions() -> None:
                           f"(Wallet: {network.nodes[j].wallet.wallet_id})")
 
 
-def animate() -> None:
+def animate(frame) -> None:
     global initial_positions, network, transactions, transaction_timestamps, witness_timestamps
     global ax, fig, sc
 
